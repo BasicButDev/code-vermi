1f1261fbcdc667ef3a3b5243839b1826a79fced2
A based user ? <chevq@outlook.com>
2026-04-02 14:14:32 +0200

    Update code_temp__humi.ino
    Fix delays in loop()

--- code_temp__humi.ino
+++ code_temp__humi.ino
@@ -17,6 +17,7 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);                         // Définition de
 bool initd = true;
 float h = 30;
 float t = 30;
+float time_since_init = 0;
 
 /*const int color R = 255; // initialisation de la couleur rouge à 255
 const int colorG = 0;   // initialisation de la couleur verte à 0
@@ -37,19 +38,14 @@ void setup() {
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
 
 void loop() {
+ EVERY_N_MILLISECONDS( 1 ) {
   if (initd == true) {                                         // Tempo quand init
     digitalWrite(13, LOW);                                     // envoie pas le courant dans pin 13
-    delay(2500);                                               // Délai de tempo
+    delay(750);                                                // Délai de tempo
     initd = false;                                             // Pour exec 1x
   }
   float h = dht.readHumidity();                                //définition d'un réel h qui a pour valeur l'humidité mesurée
   float t = dht.readTemperature();                             //définition d'un réel t qui a pour valeur la température mesurée
-  
-  if (h > 34) {
-    digitalWrite(13, HIGH);                                    // envoie le courant dans pin 13
-    delay(2500);                                               // Tempo pour pas cramer le relais
-  }
-  else digitalWrite(13, LOW);  
   
   Serial.print("Humidity : ");
   Serial.println(h);                                           // affichage de l'humidité dans le moniteur série 
@@ -57,15 +53,17 @@ void loop() {
   Serial.println(t);                                           // affichage de la température dans le moniteur série
 
   //lcd.clear();
-  lcd.setCursor(0, 0);                                         //***********************************************************
-  lcd.print("Temp = ");                                        //
-  lcd.print(t);                                                //           Affiche les données temp et hum
-  lcd.print(" *C ");                                           //                     sur la LCD
-  lcd.setCursor(0, 1);                                         //
-  lcd.print(" Hum = ");                                        //
-  lcd.print(h);                                                //
-  lcd.print(" % ");                                            //***********************************************************
-  delay(500);    // attendre 500 ms
+  if (time_since_init % 500 == 0){
+    lcd.setCursor(0, 0);                                         //***********************************************************
+    lcd.print("Temp = ");                                        //
+    lcd.print(t);                                                //           Affiche les données temp et hum
+    lcd.print(" *C ");                                           //                     sur la LCD
+    lcd.setCursor(0, 1);                                         //
+    lcd.print(" Hum = ");                                        //
+    lcd.print(h);                                                //
+    lcd.print(" % ");                                            //***********************************************************
+    }
+  //delay(500);    // attendre 500 ms
 
   // Call the current pattern function once, updating the 'leds' array  
   if (h > 37) fill_solid(leds, NUM_LEDS, CRGB::Red);
@@ -78,4 +76,10 @@ void loop() {
 
   // do some periodic updates
   EVERY_N_MILLISECONDS( 10 ) { gHue=gHue+2; } // slowly cycle the "base color" through the rainbow
+ }
+ EVERY_N_MILLISECONDS( 1 ) { time_since_init++; } //get time i guess ????
+ EVERY_N_MILLISECONDS( 1500 ) {
+  if (h > 33) digitalWrite(13, HIGH);                                    // envoie le courant dans pin 13
+  else digitalWrite(13, LOW);  }
+  
 }
